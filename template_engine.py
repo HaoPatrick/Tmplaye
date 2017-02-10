5fd60e2d2144a90d6900f1b140d1583d78b162e3
Hao Xiangpeng <3150102255@zju.edu.cn>
2017-02-10 10:54:13 +0800

    finished yet

--- template_engine.py
+++ template_engine.py
@@ -1,6 +1,10 @@
 import re
 
 
+class TempliteSyntaxError(ValueError):
+    pass
+
+
 class CodeBuilder(object):
     """Build source code conveniently."""
 
@@ -93,3 +97,85 @@ class Templite(object):
                 elif words[0] == 'for':
                     if len(words) != 4 or words[2] != 'in':
                         self._syntax_error("Don't understand for", token)
+                    ops_stack.append("for")
+                    self._variable(words[1], self.loop_vars)
+                    code.add_line(
+                        "for c_%s in %s:" % (
+                            words[1], self._expr_code(words[3])
+                        )
+                    )
+                    code.indent()
+                elif words[0].startswith("end"):
+                    if len(words) != 1:
+                        self._syntax_error("Don't understand end", token)
+                    end_what = words[0][:3]
+                    if not ops_stack:
+                        self._syntax_error("Too many ends", token)
+                    start_what = ops_stack.pop()
+                    if start_what != end_what:
+                        self._syntax_error("Mismatched end tag", end_what)
+                    code.dedent()
+                else:
+                    self._syntax_error("Don't understand tag", words[0])
+            else:
+                if token:
+                    buffered.append(repr(token))
+        if ops_stack:
+            self._syntax_error("Unmatched action tag", ops_stack[-1])
+        flush_output()
+        for var_name in self.all_vars - self.loop_vars:
+            vars_code.add_line("c_%s = context[%r]" % (var_name, var_name))
+        code.add_line("return ''.join(result)")
+        code.dedent()
+        self._render_function = code.get_globals()['render_function']
+
+    def _expr_code(self, expr):
+        """Generate a Python expression for `expr`"""
+        if "|" in expr:
+            pipes = expr.split("|")
+            code = self._expr_code(pipes[0])
+            for func in pipes[1:]:
+                self._variable(func, self.all_vars)
+                code = "c_%s(%s)" % (func, code)
+        elif "." in expr:
+            dots = expr.split(".")
+            code = self._expr_code(dots[0])
+            args = ", ".join(repr(d) for d in dots[1:])
+            code = "do_dots(%s, %s)" % (code, args)
+        else:
+            self._variable(expr, self.all_vars)
+            code = "c_%s" % expr
+        return code
+
+    def _syntax_error(self, msg, thing):
+        """Raise a syntax error using `msg`, and showing `thing`."""
+        raise TempliteSyntaxError("%s: %r" % (msg, thing))
+
+    def _variable(self, name, vars_set):
+        """Track that `name` is used as variable.
+        Adds the name to `vars_set`, a set of variable names.
+        Raise an syntax error if `name` is not a valid name.
+        """
+        if not re.match(r"[_a-zA-Z][_a-zA-Z0-9]*$", name):
+            self._syntax_error("Not a valid name", name)
+        vars_set.add(name)
+
+    def render(self, context=None):
+        """Render this tempalte by applying it to `context`.
+        `context` is a dictionary of values to use in this rendering.
+        """
+        render_context = dict(self.context)
+        if context:
+            render_context.update(context)
+        return self._render_function(render_context, self._do_dots)
+
+    def _do_dots(self, value, *dots):
+        """Evaluate dotted expressions at runtime"""
+        for dot in dots:
+            try:
+                value = getattr(value, dot)
+            except AttributeError:
+                value = value[dot]
+            if callable(value):
+                value = value()
+        return value
